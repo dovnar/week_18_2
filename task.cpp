e0d6571f88e7225fb504bbb58bfcfa0d6ff61a69
setlol <dovnar.maxim@gmail.com>
2016-10-09 21:26:25 +0300

    Add files via upload

--- task.cpp
+++ task.cpp
@@ -1,126 +1,98 @@
 ﻿#include <iostream>
 using namespace std;
 
-void initializationAndFillArray(int*** arr, int& countTab, int& countLine, int& size);
-void printArray(int*** arr, int& countTab, int& countLine, int& size);
+int** initializationAndFillArray(int& countLine, int& size);
+void printArray(int** arr, int countLine, int size);
 void printArray(int* arr, int size);
-void deleteArray(int*** arr, int countTab, int countLine, int size);
-void createArrayBasicValuesForABC(int*** a, int countTabA, int countLineA, int sizeA,
-	int*** b,	int countTabB, int countLineB, int sizeB, int*** c, int countTabC, int countLineC, int sizeC);
-void createArrayUniqueValuesForABC(int*** a, int countTabA, int countLineA, int sizeA,
-	int*** b, int countTabB, int countLineB, int sizeB, int*** c, int countTabC, int countLineC, int sizeC);
-void createArrayBasicValuesForAC(int*** a, int countTabA, int countLineA, int sizeA,
-	int *** c, int countTabC, int countLineC, int sizeC);
-void createArrayNegativeValuesForABC(int*** a, int countTabA, int countLineA, int sizeA,
-	int*** b, int countTabB, int countLineB, int sizeB, int*** c, int countTabC, int countLineC, int sizeC);
+void deleteArray(int** arr, int countLine, int size);
+void createArrayBasicValuesForABC(int** a, int countLineA, int sizeA,
+	int** b,	int countLineB, int sizeB, int** c, int countLineC, int sizeC);
+void createArrayUniqueValuesForABC(int** a, int countLineA, int sizeA,
+	int** b, int countLineB, int sizeB, int** c, int countLineC, int sizeC);
+void createArrayBasicValuesForAC(int** a, int countLineA, int sizeA,
+	int** c, int countLineC, int sizeC);
+void createArrayNegativeValuesForABC(int** a, int countLineA, int sizeA,
+	int** b, int countLineB, int sizeB, int** c, int countLineC, int sizeC);
 
 int main()
 {
-	int*** a;
-	int countTabA;
-	int countLineA;
-	int sizeA;
-	int*** b;
-	int countTabB;
-	int countLineB;
-	int sizeB;
-	int*** c;
-	int countTabC;
-	int countLineC;
-	int sizeC;
+	int countLineA, sizeA;
+	int** a = initializationAndFillArray(countLineA, sizeA);
+	int countLineB, sizeB;
+	int** b = initializationAndFillArray(countLineB, sizeB);
+	int countLineC, sizeC;
+	int** c = initializationAndFillArray(countLineC, sizeC);;
+	
+	printArray(a, countLineA, sizeA);
+	printArray(b, countLineB, sizeB);
+	printArray(c, countLineC, sizeC);
 
-	initializationAndFillArray(a, countTabA, countLineA, sizeA);
-	initializationAndFillArray(b, countTabB, countLineB, sizeB);
-	initializationAndFillArray(c, countTabC, countLineC, sizeC);
+	createArrayBasicValuesForABC(a, countLineA, sizeA, b, countLineB, sizeB, c, countLineC, sizeC);
+	createArrayUniqueValuesForABC(a, countLineA, sizeA, b, countLineB, sizeB, c, countLineC, sizeC);
+	createArrayBasicValuesForAC(a, countLineA, sizeA, c, countLineC, sizeC);
+	createArrayNegativeValuesForABC(a, countLineA, sizeA, b,  countLineB, sizeB, c, countLineC, sizeC);
 
-	printArray(a, countTabA, countLineA, sizeA);
-	printArray(b, countTabB, countLineB, sizeB);
-	printArray(c, countTabC, countLineC, sizeC);
-
-	createArrayBasicValuesForABC(a, countTabA, countLineA, sizeA, b, countTabB, countLineB, sizeB, c, countTabC, countLineC, sizeC);
-	createArrayUniqueValuesForABC(a, countTabA, countLineA, sizeA, b, countTabB, countLineB, sizeB, c, countTabC, countLineC, sizeC);
-	createArrayBasicValuesForAC(a, countTabA, countLineA, sizeA, c, countTabC, countLineC, sizeC);
-	createArrayNegativeValuesForABC(a, countTabA, countLineA, sizeA, b, countTabB, countLineB, sizeB, c, countTabC, countLineC, sizeC);
-
-	deleteArray(a, countTabA, countLineA, sizeA);
-	deleteArray(b, countTabB, countLineB, sizeB);
-	deleteArray(c, countTabC, countLineC, sizeC);
+	deleteArray(a,  countLineA, sizeA);
+	deleteArray(b,  countLineB, sizeB);
+	deleteArray(c, countLineC, sizeC);
 }
 
-void initializationAndFillArray(int *** arr, int & countTab, int & countLine, int & size)
+int** initializationAndFillArray(int& countLine, int& size)
 {
-	cout << "enter tab: ";
-	cin >> countTab;
 	cout << "enter line: ";
 	cin >> countLine;
 	cout << "enter size: ";
 	cin >> size;
-	arr = new int**[countTab];
-	for (int i = 0; i < countTab; i++)
+	int** arr = new int*[countLine];
+	for (int i = 0; i < countLine; i++)
 	{
-		(arr[i]) = new int*[countLine];
-		for (int j = 0; j < countLine; j++)
+		(arr[i]) = new int[size];
+		for (int j = 0; j < size; j++)
 		{
-			(arr[i][j]) = new int[size];
-			for (int l = 0; l < size; l++)
-			{
-				cout << "enter number: ";
-				cin >> arr[i][j][l];
-			}
+			cout << "enter number: ";
+			cin >> arr[i][j];
 		}
 	}
+	return arr;
 }
 
-void printArray(int *** arr, int & countTab, int & countLine, int & size)
+void printArray(int** arr,  int countLine, int size)
 {
-	for (int i = 0; i < countTab; i++)
+	for (int i = 0; i < countLine; i++)
 	{
-		for (int j = 0; j < countLine; j++)
+		for (int j = 0; j < size; j++)
 		{
-			for (int l = 0; l < size; l++)
-			{
-				cout << arr[i][j][l] << "  ";
-			}
-			cout << endl;
+			cout << arr[i][j] << "  ";
 		}
 		cout << endl;
 	}
 	cout << endl << endl;
 }
 
-void createArrayBasicValuesForABC(int *** a, int countTabA, int countLineA, int sizeA, 
-	int *** b, int countTabB, 	int countLineB, int sizeB, int *** c, int countTabC, int countLineC, int sizeC)
+void createArrayBasicValuesForABC(int** a,  int countLineA, int sizeA, 
+	int** b,  	int countLineB, int sizeB, int** c,  int countLineC, int sizeC)
 {
-	int* arr;
+	int* arr = new int[1]{ NULL };
 	int size = 0;
-	for (int tabA = 0; tabA < countTabA; tabA++)
+	for (int lineA = 0; lineA < countLineA; lineA++)
 	{
-		for (int lineA = 0; lineA < countLineA; lineA++)
+		for (int iA = 0; iA < sizeA; iA++)
 		{
-			for (int iA = 0; iA < sizeA; iA++)
+			for (int lineB = 0; lineB < countLineB; lineB++)
 			{
-				for (int tabB = 0; tabB < countTabB; tabB++)
+				for (int iB = 0; iB < sizeB; iB++)
 				{
-					for (int lineB = 0; lineB < countLineB; lineB++)
+					if (a[lineA][iA] == b[lineB][iB])
 					{
-						for (int iB = 0; iB < sizeB; iB++)
+						for (int lineC = 0; lineC < countLineC; lineC++)
 						{
-							if (a[tabA][lineA][iA] == b[tabB][lineB][iB])
+							for (int iC = 0; iC < sizeC; iC++)
 							{
-								for (int tabC = 0; tabC < countTabC; tabC++)
+								if (a[lineA][iA] == c[lineC][iC])
 								{
-									for (int lineC = 0; lineC < countLineC; lineC++)
-									{
-										for (int iC = 0; iC < sizeC; iC++)
-										{
-											if (a[tabA][lineA][iA] == c[tabC][lineC][iC])
-											{
-												size++;
-												arr = new int[size];
-												arr[size-1] = a[tabA][lineA][iA];
-											}
-										}
-									}
+									size++;
+									arr = new int[size];
+									arr[size-1] = a[lineA][iA];
 								}
 							}
 						}
@@ -134,144 +106,107 @@ void createArrayBasicValuesForABC(int *** a, int countTabA, int countLineA, int 
 	delete[] arr;
 }
 
-void createArrayUniqueValuesForABC(int *** a, int countTabA, int countLineA, int sizeA, 
-	int *** b, int countTabB, int countLineB, int sizeB, int *** c, int countTabC, int countLineC, int sizeC)
+void createArrayUniqueValuesForABC(int** a,  int countLineA, int sizeA, 
+	int** b, int countLineB, int sizeB, int** c, int countLineC, int sizeC)
 {
-	int* arr;
+	int* arr = new int[1]{ NULL };
 	int size = 0;
 	int matches;
-	for (int tabA = 0; tabA < countTabA; tabA++)
+	for (int lineA = 0; lineA < countLineA; lineA++)
 	{
-		for (int lineA = 0; lineA < countLineA; lineA++)
+		for (int iA = 0; iA < sizeA; iA++)
 		{
-			for (int iA = 0; iA < sizeA; iA++)
-			{
-				matches = 0;
-				for (int tabB = 0; tabB < countTabB; tabB++)
+			matches = 0;
+				for (int lineB = 0; lineB < countLineB; lineB++)
 				{
-					for (int lineB = 0; lineB < countLineB; lineB++)
+					for (int iB = 0; iB < sizeB; iB++)
 					{
-						for (int iB = 0; iB < sizeB; iB++)
+						if (a[lineA][iA] == b[lineB][iB])
 						{
-							if (a[tabA][lineA][iA] == b[tabB][lineB][iB])
+							matches++;
+						}
+					}
+				}
+			if (matches == 0)
+			{
+					for (int lineC = 0; lineC < countLineC; lineC++)
+					{
+						for (int iC = 0; iC < sizeC; iC++)
+						{
+							if (a[lineA][iA] == c[lineC][iC])
 							{
 								matches++;
 							}
 						}
 					}
 				}
-				if (matches == 0)
-				{
-					for (int tabC = 0; tabC < countTabC; tabC++)
-					{
-						for (int lineC = 0; lineC < countLineC; lineC++)
-						{
-							for (int iC = 0; iC < sizeC; iC++)
-							{
-								if (a[tabA][lineA][iA] == c[tabC][lineC][iC])
-								{
-									matches++;
-								}
-							}
-						}
-					}
-				}
-				if (matches == 0)
-				{
-					size++;
-					arr = new int[size];
-					arr[size - 1] = a[tabA][lineA][iA];
-				}
+			if (matches == 0)
+			{
+				size++;
+				arr = new int[size];
+				arr[size - 1] = a[lineA][iA];
 			}
 		}
 	}
-	for (int tabB = 0; tabB < countTabB; tabB++)
+	for (int lineB = 0; lineB < countLineB; lineB++)
 	{
-		for (int lineB = 0; lineB < countLineB; lineB++)
+		for (int iB = 0; iB < sizeB; iB++)
 		{
-			for (int iB = 0; iB < sizeB; iB++)
+			matches = 0;
+			for (int lineA = 0; lineA < countLineA; lineA++)
 			{
-				matches = 0;
-				for (int tabA = 0; tabA < countTabA; tabA++)
+				for (int iA = 0; iA < sizeA; iA++)
 				{
-					for (int lineA = 0; lineA < countLineA; lineA++)
+					if (a[lineA][iA] == b[lineB][iB])
 					{
-						for (int iA = 0; iA < sizeA; iA++)
+						matches++;
+					}
+				}
+			}
+			if (matches == 0)
+			{
+				for (int lineC = 0; lineC < countLineC; lineC++)
+				{
+					for (int iC = 0; iC < sizeC; iC++)
+					{
+						if (b[lineB][iB] == c[lineC][iC])
 						{
-							if (a[tabA][lineA][iA] == b[tabB][lineB][iB])
-							{
-								matches++;
-							}
+							matches++;
 						}
 					}
 				}
-				if (matches == 0)
-				{
-					for (int tabC = 0; tabC < countTabC; tabC++)
-					{
-						for (int lineC = 0; lineC < countLineC; lineC++)
-						{
-							for (int iC = 0; iC < sizeC; iC++)
-							{
-								if (b[tabB][lineB][iB] == c[tabC][lineC][iC])
-								{
-									matches++;
-								}
-							}
-						}
-					}
-				}
-				if (matches == 0)
-				{
-					size++;
-					arr = new int[size];
-					arr[size - 1] = b[tabB][lineB][iB];
-				}
+			}
+			if (matches == 0)
+			{
+				size++;
+				arr = new int[size];
+				arr[size - 1] = b[lineB][iB];
 			}
 		}
 	}
-	for (int tabC = 0; tabC < countTabC; tabC++)
+	for (int lineC = 0; lineC < countLineC; lineC++)
 	{
-		for (int lineC = 0; lineC < countLineC; lineC++)
+		for (int iC = 0; iC < sizeC; iC++)
 		{
-			for (int iC = 0; iC < sizeC; iC++)
+			matches = 0;
+			if (matches == 0)
 			{
-				matches = 0;
-				for (int tabB = 0; tabB < countTabB; tabB++)
+				for (int lineA = 0; lineA < countLineA; lineA++)
 				{
-					for (int lineB = 0; lineB < countLineB; lineB++)
+					for (int iA = 0; iA < sizeA; iA++)
 					{
-						for (int iB = 0; iB < sizeB; iB++)
+						if (a[lineA][iA] == c[lineC][iC])
 						{
-							if (b[tabB][lineB][iB] == c[tabC][lineC][iC])
-							{
-								matches++;
-							}
+							matches++;
 						}
 					}
 				}
-				if (matches == 0)
-				{
-					for (int tabA = 0; tabA < countTabA; tabA++)
-					{
-						for (int lineA = 0; lineA < countLineA; lineA++)
-						{
-							for (int iA = 0; iA < sizeA; iA++)
-							{
-								if (a[tabA][lineA][iA] == c[tabC][lineC][iC])
-								{
-									matches++;
-								}
-							}
-						}
-					}
-				}
-				if (matches == 0)
-				{
-					size++;
-					arr = new int[size];
-					arr[size - 1] = c[tabC][lineC][iC];
-				}
+			}
+			if (matches == 0)
+			{
+				size++;
+				arr = new int[size];
+				arr[size - 1] = c[lineC][iC];
 			}
 		}
 	}
@@ -280,30 +215,25 @@ void createArrayUniqueValuesForABC(int *** a, int countTabA, int countLineA, int
 	delete[] arr;
 }
 
-void createArrayBasicValuesForAC(int *** a, int countTabA, int countLineA, int sizeA, 
-	int *** c, int countTabC, int countLineC, int sizeC)
+void createArrayBasicValuesForAC(int** a, int countLineA, int sizeA, 
+	int** c, int countLineC, int sizeC)
 {
-	int* arr;
+	int* arr = new int[1]{ NULL };
 	int size = 0;
-	for (int tabA = 0; tabA < countTabA; tabA++)
+	
+	for (int lineA = 0; lineA < countLineA; lineA++)
 	{
-		for (int lineA = 0; lineA < countLineA; lineA++)
+		for (int iA = 0; iA < sizeA; iA++)
 		{
-			for (int iA = 0; iA < sizeA; iA++)
+			for (int lineC = 0; lineC < countLineC; lineC++)
 			{
-				for (int tabC = 0; tabC < countTabC; tabC++)
+				for (int iC = 0; iC < sizeC; iC++)
 				{
-					for (int lineC = 0; lineC < countLineC; lineC++)
+					if (a[lineA][iA] == c[lineC][iC])
 					{
-						for (int iC = 0; iC < sizeC; iC++)
-						{
-							if (a[tabA][lineA][iA] == c[tabC][lineC][iC])
-							{
-								size++;
-								arr = new int[size];
-								arr[size - 1] = a[tabA][lineA][iA];
-							}
-						}
+						size++;
+						arr = new int[size];
+						arr[size - 1] = a[lineA][iA];
 					}
 				}
 			}
@@ -314,86 +244,78 @@ void createArrayBasicValuesForAC(int *** a, int countTabA, int countLineA, int s
 	delete[] arr;
 }
 
-void createArrayNegativeValuesForABC(int *** a, int countTabA, int countLineA, int sizeA, 
-	int *** b, int countTabB, int countLineB, int sizeB, int *** c, int countTabC, int countLineC, int sizeC)
+void createArrayNegativeValuesForABC(int** a, int countLineA, int sizeA, 
+	int** b, int countLineB, int sizeB, int** c, int countLineC, int sizeC)
 {
-	int* arr;
+	int* arr = new int[1]{ NULL };
 	int size = 1;
 	int matches;
-	for (int tab = 0; tab < countTabA; tab++)
+	
+	for (int line = 0; line < countLineA; line++)
 	{
-		for (int line = 0; line < countLineA; line++)
+		for (int i = 0; i < sizeA; i++)
 		{
-			for (int i = 0; i < sizeA; i++)
+			matches = 0;
+			if (a[line][i] < 0)
 			{
-				matches = 0;
-				if (a[tab][line][i] < 0)
+				for (int j = 0; j < size; j++)
 				{
-					for (int j = 0; j < size; j++)
+					if (a[line][i] == arr[j])
 					{
-						if (a[tab][line][i] == arr[j])
-						{
-							matches++;
-						}
+						matches++;
 					}
-					if (matches == 0)
-					{
-						size++;
-						arr = new int[size];
-						arr[size - 1] = a[tab][line][i];
-					}
+				}
+				if (matches == 0)
+				{
+					size++;
+					arr = new int[size];
+					arr[size - 1] = a[line][i];
 				}
 			}
 		}
 	}
-	for (int tab = 0; tab < countTabB; tab++)
+	for (int line = 0; line < countLineB; line++)
 	{
-		for (int line = 0; line < countLineB; line++)
+		for (int i = 0; i < sizeB; i++)
 		{
-			for (int i = 0; i < sizeB; i++)
+			matches = 0;
+			if (b[line][i] < 0)
 			{
-				matches = 0;
-				if (b[tab][line][i] < 0)
+				for (int j = 0; j < size; j++)
 				{
-					for (int j = 0; j < size; j++)
+					if (b[line][i] == arr[j])
 					{
-						if (b[tab][line][i] == arr[j])
-						{
-							matches++;
-						}
+						matches++;
 					}
-					if (matches == 0)
-					{
-						size++;
-						arr = new int[size];
-						arr[size - 1] = b[tab][line][i];
-					}
+				}
+				if (matches == 0)
+				{
+					size++;
+					arr = new int[size];
+					arr[size - 1] = b[line][i];
 				}
 			}
 		}
 	}
-	for (int tab = 0; tab < countTabC; tab++)
+	for (int line = 0; line < countLineC; line++)
 	{
-		for (int line = 0; line < countLineC; line++)
+		for (int i = 0; i < sizeC; i++)
 		{
-			for (int i = 0; i < sizeC; i++)
+			matches = 0;
+			if (c[line][i] < 0)
 			{
-				matches = 0;
-				if (c[tab][line][i] < 0)
+				for (int j = 0; j < size; j++)
 				{
-					for (int j = 0; j < size; j++)
+					if (c[line][i] == arr[j])
 					{
-						if (c[tab][line][i] == arr[j])
-						{
-							matches++;
-						}
+						matches++;
 					}
-					if (matches == 0)
-					{
-						size++;
-						arr = new int[size];
-						arr[size - 1] = c[tab][line][i];
-					}
+				}
+				if (matches == 0)
+				{
+					size++;
+					arr = new int[size];
+					arr[size - 1] = c[line][i];
 				}
 			}
 		}
@@ -403,7 +325,7 @@ void createArrayNegativeValuesForABC(int *** a, int countTabA, int countLineA, i
 	delete[] arr;
 }
 
-void printArray(int * arr, int size)
+void printArray(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -412,15 +334,11 @@ void printArray(int * arr, int size)
 	cout << endl << endl;
 }
 
-void deleteArray(int *** arr, int countTab, int countLine, int size)
+void deleteArray(int** arr, int countLine, int size)
 {
-	for (int tab = 0; tab < countTab; tab++)
+	for (int line = 0; line < countLine; line++)
 	{
-		for (int line = 0; line < countLine; line++)
-		{
-			delete[] arr[tab][line];
-		}
-		delete[] arr[tab];
+		delete[] arr[line];
 	}
 	delete[] arr;
 }
